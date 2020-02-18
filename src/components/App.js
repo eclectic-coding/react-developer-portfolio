6e8c4190eeaf6cd1834a4174ec3e7b2728dcc89b
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-02-18 08:00:41 -0500

    Initial commits
    # Why is this change needed?
    Initial commit. 
    # How does it address the issue?
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from App.js</h1>
+      <p>Using ParcelJS Bundler</p>
+    </div>
+  )
+}
+
+export default App
