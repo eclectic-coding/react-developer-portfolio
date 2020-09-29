79542bab51cdb79584ae9015d6c8c5c0a333a2d9
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-09-29 19:18:33 -0400

    Fix missing social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,20 +13,36 @@ const Footer = () => {
       <div className="container">
         <div className="content has-text-centered">
           <div className="has-padding-15">
-            <a href="/" target="_blank" aria-label="Chuck's DEV Blog.">
+            <a
+              href="https://dev.to/eclecticcoding"
+              target="_blank"
+              aria-label="Chuck's DEV Blog."
+            >
               <FontAwesomeIcon className="soc soc__dev" icon={faDev} size="lg" />
             </a>
-            <a href="/" target="_blank" aria-label="Chuck's GitHub.">
+            <a
+              href="https://github.com/eclectic-coding"
+              target="_blank"
+              aria-label="Chuck's GitHub."
+            >
               <FontAwesomeIcon className="soc soc__github" icon={faGithub} size="lg" />
             </a>
-            <a href="/" target="_blank" aria-label="Chuck's LinkedIn.">
+            <a
+              href="https://www.linkedin.com/in/dev-chuck-smith/"
+              target="_blank"
+              aria-label="Chuck's LinkedIn."
+            >
               <FontAwesomeIcon
                 className="soc soc__linkedin"
                 icon={faLinkedin}
                 size="lg"
               />
             </a>
-            <a href="/" target="_blank" aria-label="Chuck's Twitter.">
+            <a
+              href="https://twitter.com/EclecticCoding"
+              target="_blank"
+              aria-label="Chuck's Twitter."
+            >
               <FontAwesomeIcon className="soc soc__twitter" icon={faTwitter} size="lg" />
             </a>
           </div>
