16e65393e8e5d1858321e4edbb2d4e79fdc3eba2
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-03-29 12:17:29 -0400

    Upgrade packages

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,7 +5,7 @@ const CallToAction = () => {
     <div className="box cta">
       <p className="has-text-centered">
         <a
-          href="https://docs.google.com/document/d/e/2PACX-1vQW5N7CLjPswANhnDvMWXnjp4FN5YWDICjJWk7B2kzTqRN3F9zEmChUe2LDDOw3OqAZc5goT8btnUBY/pub"
+          href="https://docs.google.com/document/d/1VNo4IPdI4AK2PSudLcwb5Mv01qsw6sa9H7ZILhzs1tY/edit"
           target="_blank"
         >
           <span className="tag is-primary">Resume</span>
